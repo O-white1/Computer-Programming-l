3dd4153a4d1d3203617768e60677f460b90b878d
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-01-02 13:43:38 -0600

    1/2/24 1:43 final of Comp-Prog1

--- Whack-A-Mole/MainForm.py
+++ Whack-A-Mole/MainForm.py
@@ -9,21 +9,26 @@ class MainForm(Form):
 		self.InitializeComponent()
 	
 	def InitializeComponent(self):
+		self._components = System.ComponentModel.Container()
 		resources = System.Resources.ResourceManager("Whack_A_Mole.MainForm", System.Reflection.Assembly.GetEntryAssembly())
 		self._button1 = System.Windows.Forms.Button()
-		self._button2 = System.Windows.Forms.Button()
+		self._TL = System.Windows.Forms.Button()
 		self._pictureBox1 = System.Windows.Forms.PictureBox()
 		self._pictureBox2 = System.Windows.Forms.PictureBox()
-		self._button3 = System.Windows.Forms.Button()
-		self._button4 = System.Windows.Forms.Button()
-		self._button5 = System.Windows.Forms.Button()
-		self._button6 = System.Windows.Forms.Button()
-		self._button7 = System.Windows.Forms.Button()
-		self._button8 = System.Windows.Forms.Button()
-		self._button9 = System.Windows.Forms.Button()
-		self._button10 = System.Windows.Forms.Button()
+		self._TM = System.Windows.Forms.Button()
+		self._TR = System.Windows.Forms.Button()
+		self._ML = System.Windows.Forms.Button()
+		self._MM = System.Windows.Forms.Button()
+		self._MR = System.Windows.Forms.Button()
+		self._BL = System.Windows.Forms.Button()
+		self._BM = System.Windows.Forms.Button()
+		self._BR = System.Windows.Forms.Button()
 		self._label1 = System.Windows.Forms.Label()
 		self._label2 = System.Windows.Forms.Label()
+		self._timer1 = System.Windows.Forms.Timer(self._components)
+		self._label3 = System.Windows.Forms.Label()
+		self._label4 = System.Windows.Forms.Label()
+		self._label5 = System.Windows.Forms.Label()
 		self._pictureBox1.BeginInit()
 		self._pictureBox2.BeginInit()
 		self.SuspendLayout()
@@ -42,15 +47,15 @@ class MainForm(Form):
 		self._button1.UseVisualStyleBackColor = True
 		self._button1.Click += self.Button1Click
 		# 
-		# button2
+		# TL
 		# 
-		self._button2.Location = System.Drawing.Point(181, 12)
-		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(141, 109)
-		self._button2.TabIndex = 1
-		self._button2.Text = "button2"
-		self._button2.UseVisualStyleBackColor = True
-		self._button2.Visible = False
+		self._TL.Location = System.Drawing.Point(181, 12)
+		self._TL.Name = "TL"
+		self._TL.Size = System.Drawing.Size(141, 109)
+		self._TL.TabIndex = 1
+		self._TL.Text = "button2"
+		self._TL.UseVisualStyleBackColor = True
+		self._TL.Visible = False
 		# 
 		# pictureBox1
 		# 
@@ -75,85 +80,85 @@ class MainForm(Form):
 		self._pictureBox2.TabStop = False
 		self._pictureBox2.Visible = False
 		# 
-		# button3
+		# TM
 		# 
-		self._button3.Location = System.Drawing.Point(350, 12)
-		self._button3.Name = "button3"
-		self._button3.Size = System.Drawing.Size(141, 109)
-		self._button3.TabIndex = 4
-		self._button3.Text = "button3"
-		self._button3.UseVisualStyleBackColor = True
-		self._button3.Visible = False
+		self._TM.Location = System.Drawing.Point(350, 12)
+		self._TM.Name = "TM"
+		self._TM.Size = System.Drawing.Size(141, 109)
+		self._TM.TabIndex = 4
+		self._TM.Text = "button3"
+		self._TM.UseVisualStyleBackColor = True
+		self._TM.Visible = False
 		# 
-		# button4
+		# TR
 		# 
-		self._button4.Location = System.Drawing.Point(518, 12)
-		self._button4.Name = "button4"
-		self._button4.Size = System.Drawing.Size(141, 109)
-		self._button4.TabIndex = 5
-		self._button4.Text = "button4"
-		self._button4.UseVisualStyleBackColor = True
-		self._button4.Visible = False
+		self._TR.Location = System.Drawing.Point(518, 12)
+		self._TR.Name = "TR"
+		self._TR.Size = System.Drawing.Size(141, 109)
+		self._TR.TabIndex = 5
+		self._TR.Text = "button4"
+		self._TR.UseVisualStyleBackColor = True
+		self._TR.Visible = False
 		# 
-		# button5
+		# ML
 		# 
-		self._button5.Location = System.Drawing.Point(181, 158)
-		self._button5.Name = "button5"
-		self._button5.Size = System.Drawing.Size(141, 109)
-		self._button5.TabIndex = 6
-		self._button5.Text = "button5"
-		self._button5.UseVisualStyleBackColor = True
-		self._button5.Visible = False
+		self._ML.Location = System.Drawing.Point(181, 158)
+		self._ML.Name = "ML"
+		self._ML.Size = System.Drawing.Size(141, 109)
+		self._ML.TabIndex = 6
+		self._ML.Text = "button5"
+		self._ML.UseVisualStyleBackColor = True
+		self._ML.Visible = False
 		# 
-		# button6
+		# MM
 		# 
-		self._button6.Location = System.Drawing.Point(350, 158)
-		self._button6.Name = "button6"
-		self._button6.Size = System.Drawing.Size(141, 109)
-		self._button6.TabIndex = 7
-		self._button6.Text = "button6"
-		self._button6.UseVisualStyleBackColor = True
-		self._button6.Visible = False
+		self._MM.Location = System.Drawing.Point(350, 158)
+		self._MM.Name = "MM"
+		self._MM.Size = System.Drawing.Size(141, 109)
+		self._MM.TabIndex = 7
+		self._MM.Text = "button6"
+		self._MM.UseVisualStyleBackColor = True
+		self._MM.Visible = False
 		# 
-		# button7
+		# MR
 		# 
-		self._button7.Location = System.Drawing.Point(518, 158)
-		self._button7.Name = "button7"
-		self._button7.Size = System.Drawing.Size(141, 109)
-		self._button7.TabIndex = 8
-		self._button7.Text = "button7"
-		self._button7.UseVisualStyleBackColor = True
-		self._button7.Visible = False
+		self._MR.Location = System.Drawing.Point(518, 158)
+		self._MR.Name = "MR"
+		self._MR.Size = System.Drawing.Size(141, 109)
+		self._MR.TabIndex = 8
+		self._MR.Text = "button7"
+		self._MR.UseVisualStyleBackColor = True
+		self._MR.Visible = False
 		# 
-		# button8
+		# BL
 		# 
-		self._button8.Location = System.Drawing.Point(181, 327)
-		self._button8.Name = "button8"
-		self._button8.Size = System.Drawing.Size(141, 109)
-		self._button8.TabIndex = 9
-		self._button8.Text = "button8"
-		self._button8.UseVisualStyleBackColor = True
-		self._button8.Visible = False
+		self._BL.Location = System.Drawing.Point(181, 315)
+		self._BL.Name = "BL"
+		self._BL.Size = System.Drawing.Size(141, 109)
+		self._BL.TabIndex = 9
+		self._BL.Text = "button8"
+		self._BL.UseVisualStyleBackColor = True
+		self._BL.Visible = False
 		# 
-		# button9
+		# BM
 		# 
-		self._button9.Location = System.Drawing.Point(350, 327)
-		self._button9.Name = "button9"
-		self._button9.Size = System.Drawing.Size(141, 109)
-		self._button9.TabIndex = 10
-		self._button9.Text = "button9"
-		self._button9.UseVisualStyleBackColor = True
-		self._button9.Visible = False
+		self._BM.Location = System.Drawing.Point(350, 315)
+		self._BM.Name = "BM"
+		self._BM.Size = System.Drawing.Size(141, 109)
+		self._BM.TabIndex = 10
+		self._BM.Text = "button9"
+		self._BM.UseVisualStyleBackColor = True
+		self._BM.Visible = False
 		# 
-		# button10
+		# BR
 		# 
-		self._button10.Location = System.Drawing.Point(518, 327)
-		self._button10.Name = "button10"
-		self._button10.Size = System.Drawing.Size(141, 109)
-		self._button10.TabIndex = 11
-		self._button10.Text = "button10"
-		self._button10.UseVisualStyleBackColor = True
-		self._button10.Visible = False
+		self._BR.Location = System.Drawing.Point(518, 315)
+		self._BR.Name = "BR"
+		self._BR.Size = System.Drawing.Size(141, 109)
+		self._BR.TabIndex = 11
+		self._BR.Text = "button10"
+		self._BR.UseVisualStyleBackColor = True
+		self._BR.Visible = False
 		# 
 		# label1
 		# 
@@ -164,6 +169,7 @@ class MainForm(Form):
 		self._label1.TabIndex = 12
 		self._label1.Text = "0"
 		self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		self._label1.Visible = False
 		# 
 		# label2
 		# 
@@ -174,43 +180,92 @@ class MainForm(Form):
 		self._label2.TabIndex = 13
 		self._label2.Text = "Score:"
 		self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		self._label2.Visible = False
+		# 
+		# timer1
+		# 
+		self._timer1.Tick += self.Timer1Tick
+		# 
+		# label3
+		# 
+		self._label3.Font = System.Drawing.Font("Microsoft Uighur", 20.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+		self._label3.Location = System.Drawing.Point(12, 9)
+		self._label3.Name = "label3"
+		self._label3.Size = System.Drawing.Size(163, 69)
+		self._label3.TabIndex = 14
+		self._label3.Text = "Whack The Mole!"
+		self._label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		self._label3.Visible = False
+		# 
+		# label4
+		# 
+		self._label4.Font = System.Drawing.Font("Microsoft Uighur", 36, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+		self._label4.Location = System.Drawing.Point(12, 67)
+		self._label4.Name = "label4"
+		self._label4.Size = System.Drawing.Size(163, 54)
+		self._label4.TabIndex = 15
+		self._label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		self._label4.Visible = False
+		# 
+		# label5
+		# 
+		self._label5.Font = System.Drawing.Font("Microsoft Uighur", 15.75, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+		self._label5.Location = System.Drawing.Point(12, 124)
+		self._label5.Name = "label5"
+		self._label5.Size = System.Drawing.Size(163, 23)
+		self._label5.TabIndex = 16
+		self._label5.Text = "Milliseconds"
+		self._label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		self._label5.Visible = False
 		# 
 		# MainForm
 		# 
 		self.BackColor = System.Drawing.Color.Silver
 		self.ClientSize = System.Drawing.Size(852, 448)
+		self.Controls.Add(self._label5)
+		self.Controls.Add(self._label4)
+		self.Controls.Add(self._label3)
 		self.Controls.Add(self._label2)
 		self.Controls.Add(self._label1)
-		self.Controls.Add(self._button10)
-		self.Controls.Add(self._button9)
-		self.Controls.Add(self._button8)
-		self.Controls.Add(self._button7)
-		self.Controls.Add(self._button6)
-		self.Controls.Add(self._button5)
-		self.Controls.Add(self._button4)
-		self.Controls.Add(self._button3)
+		self.Controls.Add(self._BR)
+		self.Controls.Add(self._BM)
+		self.Controls.Add(self._BL)
+		self.Controls.Add(self._MR)
+		self.Controls.Add(self._MM)
+		self.Controls.Add(self._ML)
+		self.Controls.Add(self._TR)
+		self.Controls.Add(self._TM)
 		self.Controls.Add(self._pictureBox2)
 		self.Controls.Add(self._pictureBox1)
-		self.Controls.Add(self._button2)
+		self.Controls.Add(self._TL)
 		self.Controls.Add(self._button1)
 		self.Name = "MainForm"
 		self.Text = "Whack-A-Mole"
 		self._pictureBox1.EndInit()
 		self._pictureBox2.EndInit()
 		self.ResumeLayout(False)
+	
 		
-		
-		
-
 
 	def Button1Click(self, sender, e):
 		self._button1.Visible = False
-		self._button2.Visible = True
-		self._button3.Visible = True
-		self._button4.Visible = True
-		self._button5.Visible = True
-		self._button6.Visible = True
-		self._button7.Visible = True
-		self._button8.Visible = True
-		self._button9.Visible = True
-		self._button10.Visible = True
+		self._TL.Visible = True
+		self._TM.Visible = True
+		self._TR.Visible = True
+		self._ML.Visible = True
+		self._MM.Visible = True
+		self._MR.Visible = True
+		self._BL.Visible = True
+		self._BM.Visible = True
+		self._BR.Visible = True
+		self._label1.Visible = True
+		self._label2.Visible = True
+		self._label3.Visible = True
+		self._label4.Visible = True
+		self._label5.Visible = True
+		self._timer1.Start()
+		TimeLeft = 2000
+
+	def Timer1Tick(self, sender, e):
+		TimeLeft -= 1
+		self._label4.Text = time
